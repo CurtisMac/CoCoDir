a3627c27c5a15045fcf10bcbd45a78751f3e1dcb
Curtis M <34104460+CurtisMac@users.noreply.github.com>
2018-04-18 17:19:22 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+//body parser
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+//allow CORS
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS')
+  next()
+})
+
+//routes
+app.get('/', (req, res)=>{
+  res.send('hi mom, this is my backend!')
+})
+
+
+module.exports = app
