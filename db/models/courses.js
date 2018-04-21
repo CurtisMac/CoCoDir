e30aab65dc16edb5c4b44cfd4add9997415df0c9
Curtis M <34104460+CurtisMac@users.noreply.github.com>
2018-04-20 22:19:37 -0700

    updated courses model

--- db/models/courses.js
+++ db/models/courses.js
@@ -1,7 +1,12 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Courses = sequelize.define('Courses', {
-    title: DataTypes.STRING
+    url: DataTypes.STRING,
+    title: DataTypes.STRING,
+    description: DataTypes.TEXT,
+    pubDate: DataTypes.DATE,
+    pubUpdate: DataTypes.DATE,
+
   }, {});
   Courses.associate = (models) => {
     // associations can be defined here
